cb4e39512ce38826dcbf3136bf021c098bff3f84
Percyking <enobongndedde@gmail.com>
2023-02-25 22:56:03 +0100

    Part not working

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -1,16 +1,30 @@
+import 'dart:convert';
+
 import 'package:rest_api_example/api_constants.dart';
 import 'package:rest_api_example/user_model.dart';
 import 'package:http/http.dart' as http;
 import 'dart:developer';
 
 class ApiService {
+//   void UserModel() async {
+//     var urlString = await http.get(Uri.https("random-data-api.com", "users"));
+//     if (urlString.statusCode == 200) {
+//       var model = jsonDecode(urlString.body);
+//       print(model)
+//     }
+//   }
+
   Future<List<UserModel>?> getUsers() async {
     try {
-      var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndPoint);
-      var response = await http.get(url);
-      if (response.statusCode == 200) {
-        List<UserModel> _model = UserModelFromJson(response.body);
-        return _model;
+      var urlString = await http.get(Uri.https("random-data-api.com", "users"));
+
+      //var response = await http.get(url);
+      if (urlString.statusCode == 200) {
+        var model = jsonDecode(urlString.body);
+        print(model);
+
+        //List<UserModel> _model = UserModelFromJson(response.body);
+        //return _model;
       }
     } catch (e) {
       log(e.toString());
